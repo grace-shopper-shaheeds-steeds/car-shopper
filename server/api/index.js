94287905b68e5f98bb6c9fdd852e97f9b7a8d659
Shaheed Shamsi <shamsishaheed@gmail.com>
2018-06-04 13:25:46 -0500

    shaheed test

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-
+console.log('shaheed testing branch')
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
