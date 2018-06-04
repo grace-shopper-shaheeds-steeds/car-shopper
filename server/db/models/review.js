4a59339dc38f6468d8982ec5ef5f7c5db22fe7c8
Shaheed Shamsi <shamsishaheed@gmail.com>
2018-06-04 15:59:19 -0500

    product and review models complete

---
+++ server/db/models/review.js
@@ -0,0 +1,13 @@
+const db = require('./db.js')
+const Sequelize = require('sequelize')
+
+const Review = db.define('review', {
+  content: {
+    type: Sequelize.TEXT,
+  },
+  rating: {
+    type: Sequelize.FLOAT
+  }
+})
+
+module.exports = Review
