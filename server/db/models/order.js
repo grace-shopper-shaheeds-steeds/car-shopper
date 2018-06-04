130713bc4588e4147f0d471be7bde21fc58420b2
Dan Gutt <dgutt10@gmail.com>
2018-06-04 16:58:33 -0500

    fixed order and order-items

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -3,11 +3,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-  userId: {
-      type: Sequelize.INTEGER,
-      unique: false,
-      allowNull: false
-  },
   totalAmt: {
       type: Sequelize.INTEGER,
       unique: false,
