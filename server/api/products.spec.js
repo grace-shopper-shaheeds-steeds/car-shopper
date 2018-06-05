75eb510cb9dac4f2ac440a9577aa6e4a936927b8
Dan Gutt <dgutt10@gmail.com>
2018-06-05 15:48:41 -0500

    post route test

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -10,7 +10,7 @@ describe('Product routes', () => {
     return db.sync({force: true})
   })
 
-  describe('/api/products/', () => {
+  describe('GET ROUTES', () => {
     const sampleProduct = {
       title: 'Prius',
       description: 'fastest car alive',
@@ -34,4 +34,30 @@ describe('Product routes', () => {
         })
     })
   })
+
+  describe('POST ROUTES', () =>{
+    const sampleProduct2 = {
+      title: 'Prius',
+      description: 'fastest car alive',
+      price: 10.5,
+      inventoryQuantity: 1,
+      photo: 'photoTest',
+      averageRating: 3.5
+    }
+    it('POST /api/products', async () =>{
+      await request(app)
+        .post('/api/products')
+        .send(sampleProduct2)
+        .expect(200)
+        const newProduct = await Product.findOne({
+          where:{
+            title: 'Prius'
+          }
+        })
+        expect(!!newProduct).to.be.equal(true)
+    })
+
+  })
 })
+
+
