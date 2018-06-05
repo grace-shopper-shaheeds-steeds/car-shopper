e3ec86f8a10f1a309212c2abd779fd51a1a8dc3c
Dan Gutt <dgutt10@gmail.com>
2018-06-05 13:31:53 -0500

    put and post routes for product

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,30 @@
 const express = require('express');
 const router = express.Router()
 
+router.post('/', async (req, res, next) => {
+    try{
+        const newProduct = await Product.create(req.body)
+        res.json({message: 'Created product successfully!', product: newProduct})
+    } catch (err){
+        next(err)
+    }
+})
+
+
+router.put('/:id', async (req, res, next) =>{
+    try{
+       await Product.update(
+            req.body, {
+                where: {
+                    id: req.params.id
+                }
+            })
+        res.json({message: 'Updated product successfully'})
+    } catch(err){
+        next(err)
+    }
+})
+
+
 
 module.exports = router
