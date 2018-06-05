93e8ffabe27258cb00fa4d0bb2cc914449506557
Dan Gutt <dgutt10@gmail.com>
2018-06-05 11:07:07 -0500

    imported and exported products route setup

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/products', require('./products'))
 console.log('shaheed testing branch')
 router.use((req, res, next) => {
   const error = new Error('Not Found')
